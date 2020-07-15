ca4541a6313ef17395f70524ea939710a473a3f6
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-07-16 05:40:55 +0700

    Modul 3 - Repeating Alarm

--- MyAlarmManager/app/src/main/java/com/example/myalarmmanager/MainActivity.kt
+++ MyAlarmManager/app/src/main/java/com/example/myalarmmanager/MainActivity.kt
@@ -24,6 +24,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         btn_once_time.setOnClickListener(this)
         btn_set_once_alarm.setOnClickListener(this)
         alarmReceiver = AlarmReceiver()
+
+        btn_repeating_time.setOnClickListener(this)
+        btn_set_repeating_alarm.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -45,6 +48,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
                     onceTime,
                     onceMessage)
             }
+            R.id.btn_repeating_time -> {
+                val timePickerFragmentRepeat = TimePickerFragment()
+                timePickerFragmentRepeat.show(supportFragmentManager, TIME_PICKER_REPEAT_TAG)
+            }
+            R.id.btn_set_repeating_alarm -> {
+                val repeatTime = tv_repeating_time.text.toString()
+                val repeatMessage = edt_repeating_message.text.toString()
+                alarmReceiver.setRepeatingAlarm(this, AlarmReceiver.TYPE_REPEATING,
+                    repeatTime, repeatMessage)
+            }
         }
     }
 
@@ -61,6 +74,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
         when (tag) {
             TIME_PICKER_ONCE_TAG -> tv_once_time.text = dateFormat.format(calendar.time)
+            TIME_PICKER_REPEAT_TAG -> tv_repeating_time.text = dateFormat.format(calendar.time)
             else -> {
             }
         }
