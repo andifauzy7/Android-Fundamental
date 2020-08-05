356bd641fe5904a7e871c6de309486c76927d2c2
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-08-05 23:47:50 +0700

    Modul 4 - Consumer Content Provider

---
+++ MyNotesApp/consumerapp/src/main/java/com/example/consumerapp/db/DatabaseContract.kt
@@ -0,0 +1,26 @@
+package com.example.consumerapp.db
+
+import android.net.Uri
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class NoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_NAME = "note"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+            // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
+        }
+    }
+
+    companion object {
+        const val AUTHORITY = "com.example.mynotesapp"
+        const val SCHEME = "content"
+    }
+}
