461fef18e72035b5e3b7e6302fe28c04442ba661
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-08-05 23:03:31 +0700

    Modul 4 - Content Provider

--- MyNotesApp/app/src/main/java/com/example/mynotesapp/db/DatabaseContract.kt
+++ MyNotesApp/app/src/main/java/com/example/mynotesapp/db/DatabaseContract.kt
@@ -1,9 +1,9 @@
 package com.example.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
 internal class DatabaseContract {
-
     internal class NoteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "note"
@@ -11,6 +11,16 @@ internal class DatabaseContract {
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+            // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
+    }
+
+    companion object {
+        const val AUTHORITY = "com.example.mynotesapp"
+        const val SCHEME = "content"
     }
 }
