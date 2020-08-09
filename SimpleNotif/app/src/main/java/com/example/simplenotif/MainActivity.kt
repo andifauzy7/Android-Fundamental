18b1e75246cebaee9e0b365f5f62ee37428af850
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-08-09 18:14:30 +0700

    Modul 5 - Notif dengan PendingIntent

--- SimpleNotif/app/src/main/java/com/example/simplenotif/MainActivity.kt
+++ SimpleNotif/app/src/main/java/com/example/simplenotif/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
@@ -18,9 +22,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendNotification(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notifications_white_48px)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
                 .setContentTitle(resources.getString(R.string.content_title))
@@ -28,16 +38,21 @@ class MainActivity : AppCompatActivity() {
                 .setSubText(resources.getString(R.string.subtext))
                 .setAutoCancel(true)
 
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID,
-                    CHANNEL_NAME,
-                    NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = CHANNEL_NAME as String?
+
+            /* Create or update. */
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+
             mBuilder.setChannelId(CHANNEL_ID)
+
             mNotificationManager.createNotificationChannel(channel)
         }
 
         val notification = mBuilder.build()
+
         mNotificationManager.notify(NOTIFICATION_ID, notification)
     }
 }
