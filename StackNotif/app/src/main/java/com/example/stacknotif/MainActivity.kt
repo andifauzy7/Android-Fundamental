42193a9a7fdd6ff1fe11292fbfb4825594ce16c8
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-08-12 09:24:12 +0700

    Modul 5 - Notification Stack

--- StackNotif/app/src/main/java/com/example/stacknotif/MainActivity.kt
+++ StackNotif/app/src/main/java/com/example/stacknotif/MainActivity.kt
@@ -16,16 +16,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var idNotification = 0
+
     private val stackNotif = ArrayList<NotificationItem>()
+
     companion object {
         private const val CHANNEL_NAME = "dicoding channel"
         private const val GROUP_KEY_EMAILS = "group_key_emails"
         private const val NOTIFICATION_REQUEST_CODE = 200
         private const val MAX_NOTIFICATION = 2
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnSend.setOnClickListener {
             val sender = edtSender.text.toString()
             val message = edtMessage.text.toString()
@@ -37,11 +41,18 @@ class MainActivity : AppCompatActivity() {
                 idNotification++
                 edtSender.setText("")
                 edtMessage.setText("")
+
                 //tutup keyboard ketika tombol diklik
                 val methodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 methodManager.hideSoftInputFromWindow(edtMessage.windowToken, 0)
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        stackNotif.clear()
+        idNotification = 0
     }
 
     private fun sendNotif() {
@@ -51,46 +62,52 @@ class MainActivity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         val pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val mBuilder: NotificationCompat.Builder
+
         //Melakukan pengecekan jika idNotification lebih kecil dari Max Notif
         val CHANNEL_ID = "channel_01"
         if (idNotification < MAX_NOTIFICATION) {
             mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle("New Email from " + stackNotif[idNotification].sender)
-                    .setContentText(stackNotif[idNotification].message)
-                    .setSmallIcon(R.drawable.ic_mail_white_48px)
-                    .setLargeIcon(largeIcon)
-                    .setGroup(GROUP_KEY_EMAILS)
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true)
+                .setContentTitle("New Email from " + stackNotif[idNotification].sender)
+                .setContentText(stackNotif[idNotification].message)
+                .setSmallIcon(R.drawable.ic_mail_white_48px)
+                .setLargeIcon(largeIcon)
+                .setGroup(GROUP_KEY_EMAILS)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
         } else {
             val inboxStyle = NotificationCompat.InboxStyle()
-                    .addLine("New Email from " + stackNotif[idNotification].sender)
-                    .addLine("New Email from " + stackNotif[idNotification - 1].sender)
-                    .setBigContentTitle("$idNotification new emails")
-                    .setSummaryText("mail@dicoding")
+                .addLine("New Email from " + stackNotif[idNotification].sender)
+                .addLine("New Email from " + stackNotif[idNotification - 1].sender)
+                .setBigContentTitle("$idNotification new emails")
+                .setSummaryText("mail@dicoding")
             mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle("$idNotification new emails")
-                    .setContentText("mail@dicoding.com")
-                    .setSmallIcon(R.drawable.ic_mail_white_48px)
-                    .setGroup(GROUP_KEY_EMAILS)
-                    .setGroupSummary(true)
-                    .setContentIntent(pendingIntent)
-                    .setStyle(inboxStyle)
-                    .setAutoCancel(true)
+                .setContentTitle("$idNotification new emails")
+                .setContentText("mail@dicoding.com")
+                .setSmallIcon(R.drawable.ic_mail_white_48px)
+                .setGroup(GROUP_KEY_EMAILS)
+                .setGroupSummary(true)
+                .setContentIntent(pendingIntent)
+                .setStyle(inboxStyle)
+                .setAutoCancel(true)
         }
         /*
         Untuk android Oreo ke atas perlu menambahkan notification channel
         Materi ini akan dibahas lebih lanjut di modul extended
          */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
             /* Create or update. */
             val channel = NotificationChannel(CHANNEL_ID,
-                    CHANNEL_NAME,
-                    NotificationManager.IMPORTANCE_DEFAULT)
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT)
+
             mBuilder.setChannelId(CHANNEL_ID)
+
             mNotificationManager.createNotificationChannel(channel)
         }
+
         val notification = mBuilder.build()
+
         mNotificationManager.notify(idNotification, notification)
     }
 }
