8cc8e0e1add2ced43f6e79d3c4ffe526af6dfd37
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-10-09 18:42:06 +0700

    Alarm Manager

--- SubmissionTiga/app/src/main/java/com/example/submissiontiga/ui/setting/SettingFragment.kt
+++ SubmissionTiga/app/src/main/java/com/example/submissiontiga/ui/setting/SettingFragment.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.submissiontiga.R
+import com.example.submissiontiga.utils.AlarmReceiver
+import kotlinx.android.synthetic.main.fragment_setting.*
 
-class SettingFragment : Fragment() {
-
+class SettingFragment : Fragment(), View.OnClickListener {
+    private lateinit var alarmReceiver: AlarmReceiver
     private lateinit var settingViewModel: SettingViewModel
 
     override fun onCreateView(
@@ -23,11 +23,25 @@ class SettingFragment : Fragment() {
     ): View? {
         settingViewModel =
                 ViewModelProviders.of(this).get(SettingViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_setting, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        settingViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_cancel_repeating_alarm.setOnClickListener(this)
+        btn_set_repeating_alarm.setOnClickListener(this)
+        alarmReceiver = AlarmReceiver()
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_set_repeating_alarm -> {
+                alarmReceiver.setRepeatingAlarm(requireContext(), AlarmReceiver.TYPE_REPEATING,
+                    "09:00", resources.getString(R.string.one_time_alarm))
+            }
+            R.id.btn_cancel_repeating_alarm -> {
+                alarmReceiver.cancelAlarm(requireContext(), AlarmReceiver.TYPE_REPEATING)
+            }
+        }
     }
 }
