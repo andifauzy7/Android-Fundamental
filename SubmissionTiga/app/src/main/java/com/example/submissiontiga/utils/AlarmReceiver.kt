0534ccc9ed57535fb141002d8138cfc95843c86d
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2020-10-12 08:08:53 +0700

    Tambah fungsi buka aplikasi ketika di tap

--- SubmissionTiga/app/src/main/java/com/example/submissiontiga/utils/AlarmReceiver.kt
+++ SubmissionTiga/app/src/main/java/com/example/submissiontiga/utils/AlarmReceiver.kt
@@ -8,10 +8,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import com.example.submissiontiga.MainActivity
 import com.example.submissiontiga.R
 import java.text.ParseException
 import java.text.SimpleDateFormat
@@ -80,11 +82,14 @@ class AlarmReceiver : BroadcastReceiver() {
     }
 
     private fun showAlarmNotification(context: Context, title: String, message: String, notifId: Int) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
         val CHANNEL_ID = "Channel_1"
         val CHANNEL_NAME = "AlarmManager channel"
         val notificationManagerCompat = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.ic_access_time_black)
             .setContentTitle(title)
             .setContentText(message)
